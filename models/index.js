5218d72ff30719fa51692df3627f1b9d839c5153
Malik <malikspruill@gmail.com>
2022-05-30 12:52:44 -0400

    Added logic to models and confirmed model relationships between each other in models index.js file path

--- models/index.js
+++ models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category, {
+   foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+}) 
 
-// Tags belongToMany Products (through ProductTag)
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+
+Tag.belongsToMany(Product {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
